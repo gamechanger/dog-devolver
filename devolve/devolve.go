441b8fd614d8cea237e080fc4ad72ccebb09d912
Travis Thieman <travis.thieman@gmail.com>
2015-01-16 23:01:20 -0500

    Devolver tested and working

--- devolve/devolve.go
+++ devolve/devolve.go
@@ -1,1 +1,27 @@
 package devolve
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var dogstatsdEventPattern = regexp.MustCompile(`_e{\d+,\d+}`)
+
+// Convert a DogStatsD formatted string to its equivalent
+// in the StatsD protocol. If this is not possible, e.g.
+// if it is a DataDog event, we return "".
+func Devolve(in string) (string, error) {
+	if dogstatsdEventPattern.FindString(in) != "" {
+		return "", nil
+	}
+
+	split := strings.SplitN(in, "|", 3)
+	if len(split) == 1 {
+		return "", errors.New(fmt.Sprintf("String is not any valid *StatsD format: %s", in))
+	}
+	nameValue, metricType := split[0], split[1]
+
+	return nameValue + "|" + metricType, nil
+}
---
+++ devolve/devolve_test.go
@@ -0,0 +1,56 @@
+package devolve
+
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
+
+// wire up gocheck
+func Test(t *testing.T) { TestingT(t) }
+
+type DevolveTestSuite struct{}
+
+var _ = Suite(&DevolveTestSuite{})
+
+func (s *DevolveTestSuite) TestDevolveWithEmptyStringErrors(c *C) {
+	in := ""
+	result, err := Devolve(in)
+	c.Assert(result, Equals, "")
+	c.Assert(err, NotNil)
+}
+
+func (s *DevolveTestSuite) TestDevolveGarbageInGarbageOut(c *C) {
+	in := "I AM A POTATO"
+	result, err := Devolve(in)
+	c.Assert(result, Equals, "")
+	c.Assert(err, NotNil)
+}
+
+func (s *DevolveTestSuite) TestDevolveWithStatsDCounter(c *C) {
+	in := "my.metric:1|c"
+	result, err := Devolve(in)
+	c.Assert(result, Equals, "my.metric:1|c")
+	c.Assert(err, IsNil)
+}
+
+func (s *DevolveTestSuite) TestDevolveWithDogStatsDCounterPartial(c *C) {
+	in := "my.metric:1|c|0.1"
+	result, err := Devolve(in)
+	c.Assert(result, Equals, "my.metric:1|c")
+	c.Assert(err, IsNil)
+}
+
+func (s *DevolveTestSuite) TestDevolveWithDogStatsDCounterFull(c *C) {
+	in := "my.metric:1|c|0.1|#env:production,1420"
+	result, err := Devolve(in)
+	c.Assert(result, Equals, "my.metric:1|c")
+	c.Assert(err, IsNil)
+}
+
+func (s *DevolveTestSuite) TestDevolveWithDogStatsDEvent(c *C) {
+	in := "_e{10,50}:Cataclysm!:Literally everything exploded and you died, sorry."
+	result, err := Devolve(in)
+	c.Assert(result, Equals, "")
+	c.Assert(err, IsNil)
+}
